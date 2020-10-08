ea1a8e83716b5b6c3bcc4566ec2331d84c1b97d4
Kate Straneva <katestraneva@Kates-MBP-2.lan>
2020-10-08 10:41:08 -0400

    Basic Frontend

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home'; 
 
-class App extends Component {
-  render() {
-    return (
-      <div classname="container">
-        <h1>Ella App </h1>
-        <p>Your closet simplified</p>
-      </div>
-    );
-  }
+function App() {
+  return <Home />;
 }
+
+//class App extends Component {
+  //render() {
+    //return (
+      //<div classname="container">
+        //<h1>Ella App </h1>
+        //<p>Your closet simplified</p>
+      //</div>
+   // );
+ // }/
+//}
 
 
 export default App;

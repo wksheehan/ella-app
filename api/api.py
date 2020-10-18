3e4ad7cc4f12f5b6f5ce91ad8010541b707439cd
Daniel Berlin <berlindan8@gmail.com>
2020-10-18 15:12:55 -0400

    flask to local postgresql updates

--- api/api.py
+++ api/api.py
@@ -2,13 +2,14 @@ import time, os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 
 # Init flask
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/ella-api"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Init database
@@ -16,6 +17,9 @@ db = SQLAlchemy(app)
 
 # Init ma
 ma = Marshmallow(app)
+
+# Init migrate
+migrate = Migrate(app, db)
 
 # User class/model
 class User(db.Model):
@@ -100,11 +104,6 @@ def delete_user(id):
   db.session.commit()
 
   return user_schema.jsonify(user)
-
-# Get Time
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
 
 # Run Server
 if __name__ == '__main__':

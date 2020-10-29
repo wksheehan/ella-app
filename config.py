9adeefb08bf626d754d7b9d91f2c6c3fa88bf908
Danny Wu <daniel3wu@gmail.com>
2020-10-29 14:24:18 -0400

    Edited config.py to connect to Postgre instance on remote VM

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/ella-api"
+    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/ella-api"
+    SQLALCHEMY_DATABASE_URI = "postgresql://ella:ella@vcm-17601.vm.duke.edu:5432/testdb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False

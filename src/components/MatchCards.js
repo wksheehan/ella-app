b950d06c7c4c359d3279becdf2717dd03fcfbb51
Danny Wu <daniel3wu@gmail.com>
2020-11-14 20:35:00 -0500

    cleaned up JSON assignment for matches

--- src/components/MatchCards.js
+++ src/components/MatchCards.js
@@ -22,17 +22,8 @@ export const MatchCards = ( {matches, clothes, onDeleteMatch} ) => {
         const item2 = getClothingInfo(match.clothing_id2);
         if(item1 && item2){
           return {
-            cid1: item1.id,
-            name1: item1.name,
-            color1: item1.color,
-            occasion1: item1.occasion,
-            type1: item1.type,
-
-            cid2: item2.id,
-            name2: item2.name,
-            color2: item2.color,
-            occasion2: item2.occasion,
-            type2: item2.type,
+            item1: item1,
+            item2: item2,
           }
         }
         return null;
@@ -49,8 +40,8 @@ export const MatchCards = ( {matches, clothes, onDeleteMatch} ) => {
                   <Card key={pair.cid1}>
                       <Card.Content>
                           <Image src={process.env.PUBLIC_URL + 'ella.jpeg'}></Image>
-                          <Card.Header> {pair.name1} - {pair.name2} </Card.Header>
-                          <Card.Description> {pair.type1} - {pair.type2} </Card.Description>
+                          <Card.Header> {pair.item1.name} - {pair.item2.name} </Card.Header>
+                          <Card.Description> {pair.item1.type} - {pair.item2.type} </Card.Description>
                       </Card.Content>
                   </Card>
             );

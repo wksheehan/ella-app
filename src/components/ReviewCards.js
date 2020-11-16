da363f570dd0e28e718022e86a53ae88ed683b71
Daniel Berlin <berlindan8@gmail.com>
2020-11-16 00:13:50 -0500

    delete reviews

--- src/components/ReviewCards.js
+++ src/components/ReviewCards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, Button, Container, Image } from 'semantic-ui-react'
 
-export const ReviewCards = ( {reviews} ) => {
+export const ReviewCards = ( {reviews, onDeleteReview} ) => {
     return (
         <Card.Group itemsPerRow={6}>
         {reviews.map(review => {
@@ -13,7 +13,21 @@ export const ReviewCards = ( {reviews} ) => {
                         <Card.Description> {review.text} </Card.Description>
                     </Card.Content>
                     <Card.Content extra>
-                    {review.stars} Stars
+                        {review.stars} Stars
+                        <Button floated='right' basic color='red' onClick={async() => {
+                          const response = await fetch("/review/" + review.id, {
+                              method: 'DELETE',
+                              headers: {
+                                  'Content-type': 'application/json'
+                              },
+                              body: JSON.stringify(review)
+                          });
+                          if (response.ok) {
+                              console.log('item deleted successfully');
+                              onDeleteReview(review);
+                          }
+                        }}>
+                        Delete </Button>
                     </Card.Content>
               </Card>
             );
